effe3fffe087c8e0f02b7b68a2abd967f0266f25
Andrei Karpenko <7498329@gmail.com>
2023-05-16 00:37:23 -0700

    start project html structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <div className="header__logo"></div>
+        <div className="header__search"></div>
+        <div className="header__upload"></div>
+        <div className="header__profile"></div>
       </header>
+      <main className="main">
+        <div className="video">
+          <div className="video__image"></div>
+          <div className="video__title"></div>
+          <div className="video__data">
+            <div className="video__data-channel"></div>
+            <div className="video__data-date"></div>
+            <div className="video__data-views"></div>
+            <div className="video__data-likes"></div>
+          </div>
+          <div className="video__description"></div>
+        </div>
+        <div className="body">
+          <div className="body__comments">
+          
+          </div>
+          <div className="body__side-videos"></div>
+        </div>
+      </main>
     </div>
   );
 }
