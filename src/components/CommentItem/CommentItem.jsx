41fd17d76f39711c6aa46a62a98c5fab3be43805
Andrei Karpenko <7498329@gmail.com>
2023-05-20 00:32:31 -0700

    add utility date calculation, fix input focus styles

--- src/components/CommentItem/CommentItem.jsx
+++ src/components/CommentItem/CommentItem.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
-import "./CommentItem.scss"
+import "./CommentItem.scss";
+import { getDate, transformDateAgo } from "../../utils/utils";
 
-function CommentItem({comment: commentItem}) { // Destructure properties from the commentItem object
-    const {name, comment, timestamp} = commentItem
-    const commentDate = new Date(timestamp).toLocaleDateString('en-GB') // Convert timestamp to a localized date string
+function CommentItem({ comment: commentItem }) {
+  // Destructure properties from the commentItem object
+  const { name, comment, timestamp } = commentItem;
 
   return (
     <div className="comment-list__item">
       <div className="comment-list__item-profile"></div>
       <div className="comment-list__item-container">
         <div className="comment-list__item-container-header">
-          <div className="comment-list__item-container-name"><h3>{name}</h3></div> {/* Render the name of the commenter */}
-          <div className="comment-list__item-container-date"><h4>{commentDate}</h4></div> {/* Render the formatted comment date */}
+          <div className="comment-list__item-container-name">
+            <h3>{name}</h3>
+          </div>
+          {/* Render the name of the commenter */}
+          <div className="comment-list__item-container-date">
+            {/* <h4>{getDate(timestamp)}</h4> */}
+            <h4>{transformDateAgo(timestamp)}</h4>
+          </div>
+          {/* Render the formatted comment date */}
         </div>
-        <div className="comment-list__item-paragraph"><p>{comment}</p></div> {/* Render the comment content */}
+        <div className="comment-list__item-paragraph">
+          <p>{comment}</p>
+        </div>
+        {/* Render the comment content */}
       </div>
     </div>
   );
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -3,37 +3,27 @@ import React, { useState } from "react";
 
 import likesImage from "../../assets/icons/likes.svg";
 import viewsImage from "../../assets/icons/views.svg";
+import { getDate, transformDateAgo } from "../../utils/utils";
+
 
 function Video({ currentVideo }) {
-  const timestamp = new Date(currentVideo.timestamp);
-  const formattedDate = timestamp.toLocaleDateString('en-GB'); // Format the date as desired
+  const { title, channel, timestamp, views, likes, description } = currentVideo;
 
   return (
     <div className="video">
-      {/* <div className="video__image">
-        <video
-          controls //add video controls
-          autoPlay
-          loop
-          id=""
-          className="video__image-object"
-          poster={currentVideo.image}
-        >
-        <source src={currentVideo.video} type="video/mp4" />
-        </video>
-      </div> */}
       <div className="video__title">
-        <h1>{currentVideo.title}</h1>
+        <h1>{title}</h1>
       </div>
 
       <div className="video__data">
         <div className="video__data-container">
           <div className="video__data-container-channel">
-            <h3>By {currentVideo.channel}</h3>
+            <h3>By {channel}</h3>
           </div>
           <div className="video__data-container-date">
             <h4 className="video__data-container-date-output">
-              {formattedDate}
+              {/* {getDate(timestamp)} */}
+              {transformDateAgo(timestamp)}
             </h4>
           </div>
         </div>
@@ -43,19 +33,19 @@ function Video({ currentVideo }) {
               src={viewsImage}
               className="video__data-container-views-image"
             />
-            <h4>{currentVideo.views}</h4>
+            <h4>{views}</h4>
           </div>
           <div className="video__data-container-likes">
             <img
               src={likesImage}
               className="video__data-container-likes-image"
             />
-            <h4>{currentVideo.likes}</h4>
+            <h4>{likes}</h4>
           </div>
         </div>
       </div>
       <div className="video__description">
-        <p>{currentVideo.description}</p>
+        <p>{description}</p>
       </div>
     </div>
   );
