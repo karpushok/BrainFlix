a9fac0f6a7aee32e8d9eeccdf627acfd2c3e0af5
Andrei Karpenko <7498329@gmail.com>
2023-06-04 23:52:49 -0700

    add form upload

--- src/components/Upload/Upload copy.jsx
+++ src/components/Upload/Upload copy.jsx
@@ -1,13 +1,13 @@
 import "./Upload.scss";
-import { useState, useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { uploadData } from '../../utils/utils'
+import {uploadData} from '../../utils/utils'
 
 function Upload() {
   const [inputName, setInputName] = useState("");
-  const [inputDescription, setInputDescription] = useState("");
-  const [inputPosterImage, setInputPosterImage] = useState("")
-  const [inputPoster, setInputPoster] = useState("")
+  const [inputDescription, setInputDescription] = useState( "" );
+  const [inputPosterImage, setInputPosterImage] = useState('')
+  const [inputPoster, setInputPoster] = useState( "" )
   const [hasTouchedForm, setHasTouchedForm] = useState([false, false]); // [input, description]
 
   const navigate = useNavigate();
@@ -27,70 +27,51 @@ function Upload() {
   const handleCancelClick = () => {
     setInputName("");
     setInputDescription("");
-
     setHasTouchedForm([false, false]);
   };
 
-  const handleUploadPoster = (e) => {
+  const handleInputPoster = ( e ) => {
+    console.log( `Upload.jsx - line: 33 ->> e`, e.target.accept )
+    
     const form = new FormData( formRef.current );
     const fileReader = new FileReader();
 
-    console.log(`Upload.jsx - line: 38 ->> formRef.current`, formRef.current)
-    console.log(`Upload.jsx - line: 38 ->> form.get('file')`, form.get('file'))
-    console.log(`Upload.jsx - line: 38 ->> form.get('file')`, form.get('description'))
+    fileReader.readAsDataURL( form.get( 'file' ) );
     
-    // fileReader.readAsDataURL(form.get('file'));
-    
-    // fileReader.addEventListener('load', () => {
-    //   const base64Blob = fileReader.result;
-
-    //   console.log(`Upload.jsx - line: 43 ->> base64Blob`, base64Blob)
+    fileReader.addEventListener('load', () => {
+      const base64Blob = fileReader.result;
       
-    //   setInputPoster( e.target.value )
-    //   setInputPosterImage( base64Blob )
-    // });
-
+      setInputPoster( e.target.value )
+      setInputPosterImage(base64Blob)
+    });
   }
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
-    // console.log(`Upload.jsx - line: 40 ->> inputPosterPath`, inputPoster)
-
-    // const formData = new FormData();
-    // formData.append('file', inputPoster);
-
-    // console.log(`Upload.jsx - line: 43 ->> formData`, formData)
-
-    
     const isValidForm =
       hasTouchedForm.every((el) => el === true) &&
       inputName &&
       inputDescription &&
       inputPoster;
 
-    if (isValidForm) {
+    if ( isValidForm ) {
+      
+    const formData = new FormData(formRef.current);
 
-      // Sgring.trim()!
-
-      // uploadData({
-      //   "imgSrc": imageToUpload,
-      //   "file": inputPosterPath,
-      //   "title": inputName,
-      //   "description": inputDescription
-      // }).then(() => {
-      //   navigate("/");
-      // })
+      uploadData(formData).then(() => {
+        navigate("/");
+      })
 
     } else {
-      setHasTouchedForm((prev) => [inputName.length === 0 || prev[0] !== false, inputDescription.length === 0 || prev[1] !== false ]);
+      setHasTouchedForm((prev) => [inputName.length === 0 || prev[0] !== false, inputDescription.length === 0 || prev[1] !== false]);
     }
   };
 
   return (
     <section className="upload">
       <div className="upload__header">
-        <h1 className="upload__header-text">UPLOAD VIDEO</h1>
+        <h1 className="upload__header-text">Upload Video</h1>
       </div>
       <div className="upload__container">
         <div className="upload__hero">
@@ -107,9 +88,9 @@ function Upload() {
         </div>
 
         <form className="upload__form" onSubmit={handleFormSubmit} ref={formRef}>
-          <div className="upload__input">
-            <label htmlFor="file">UPLOAD YOUR POSTER</label>
-            <input type="file" id="file" onChange={handleUploadPoster} className="upload__button-poster" accept={"image/*"} />
+          <div>
+            <label htmlFor="file">Upload video</label>
+            <input type="file" name="file" id="file" onChange={handleInputPoster} accept={"image/*"}/>
           </div>
           
           <div className="upload__subheader">
